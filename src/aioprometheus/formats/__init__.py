f921a0f49d51e3a79540a44c0912b0aa7b0db98c
hellysmile <hellysmile@gmail.com>
2020-05-20 16:50:18 -0400

    Fix unxclicit import

--- src/aioprometheus/formats/__init__.py
+++ src/aioprometheus/formats/__init__.py
@@ -2,6 +2,6 @@ from .base import IFormatter
 from . import text
 
 try:
-    from .binary import BinaryFormatter, BINARY_CONTENT_TYPE, BINARY_ACCEPTS
+    from . import binary
 except ImportError:
     binary = None
